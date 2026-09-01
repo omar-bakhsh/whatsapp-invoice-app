9cd8e6623b6dea0e05d047060e626631dcc05453
omar-bakhsh <omar.k.b222@gmail.com>
2026-09-01 13:41:23 +0300

    Enable CORS for express and socket.io to fix 401 error

--- server.js
+++ server.js
@@ -12,8 +12,14 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
